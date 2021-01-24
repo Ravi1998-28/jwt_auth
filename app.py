85ffe47605b2e674a63b072445c452c42876224e
Ravi1998-28 <68372776+Ravi1998-28@users.noreply.github.com>
2021-01-24 22:33:46 +0530

    apikey added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask ,jsonify , request
+from flask import Flask ,jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
     jwt_refresh_token_required, create_refresh_token,
@@ -7,13 +8,27 @@ from flask_jwt_extended import (
 import jwt as pyJwt
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite://///home/ravi/PycharmProjects/secure_api/library.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['JWT_SECRET_KEY'] = 'super-secret'
+
 jwt = JWTManager(app)
+db = SQLAlchemy(app)
 
 
-@app.route('/createtoken', methods=['POST'])
-def CreateToken():
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user = db.Column(db.String(50))
+    apiKey = db.Column(db.String(50))
+    created_at = db.Column(db.DateTime, default=db.func.now())
+
+
+
+@app.route('/generateToken', methods=['POST'])
+def generateToken():
     master = request.get_json()['master']
+    print(master)
     if not master:
         return jsonify({'message': 'Token is missing'}), 403
 
@@ -21,7 +36,6 @@ def CreateToken():
 
         decoded = pyJwt.decode(master, options={"verify_signature": False})
         user_hash = dict(decoded).get('id', None)
-
     except:
         return jsonify({'message': 'token is invalid'}), 403
 
@@ -37,36 +51,53 @@ def CreateToken():
 @app.route('/refresh', methods=['POST'])
 @jwt_refresh_token_required
 def refresh():
-
-    refreshToken = request.get_json()['refreshToken']
-    if not refreshToken:
-        return jsonify({'message': 'Token is missing'}), 403
-
     try:
-        decoded = pyJwt.decode(refreshToken, options={"verify_signature": False})
-        identity = dict(decoded).get('identity', None)
-        print('identity', identity)
+        current_user = get_jwt_identity()
         ret = {
-            'authToken': create_access_token(identity=identity)
+            'authToken': create_access_token(identity=current_user)
         }
         return jsonify(ret), 200
     except:
-        return jsonify({'message': 'Refresh token is invalid'}), 401
-
+        return jsonify({'message': 'token is invalid'}), 403
 
 
 @app.route('/protected', methods=['POST'])
 @jwt_required
 def protected():
-    authToken = request.get_json()['authToken']
-    if not authToken:
+    username = get_jwt_identity()
+    return jsonify(logged_in_as=username), 200
+
+
+@app.route('/ApiKeySave', methods=['GET', 'POST'])
+@jwt_required
+def ApiKey_Save():
+
+    data = request.get_json()
+    Api_Key = str(data['apiKey'])
+    master = data['master']
+    print('Api_Key:', Api_Key)
+    print('master:', master)
+    if not Api_Key:
+        return jsonify({'message': 'Api Key is missing'}), 403
+    elif not master:
         return jsonify({'message': 'Token is missing'}), 403
     try:
-        decoded = pyJwt.decode(authToken, options={"verify_signature": False})
-        username = dict(decoded).get('identity', None)
-        return jsonify(logged_in_as=username), 200
+        decoded = pyJwt.decode(master, options={"verify_signature": False})
+        username = dict(decoded).get('id', None)
+
     except:
-        return jsonify({'message': 'token is invalid'}), 401
+        return jsonify({'message': 'token is invalid'}), 403
+
+
+    new_user = Users(user=username, apiKey=Api_Key)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({
+                    "success": "true",
+                    "message": "Apikey saved",
+                    })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
